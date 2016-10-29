b8f3c6961ea55c4e246a83c46f84fe9a555ab85f
sh999 <sh999gh@gmail.com>
2016-10-29 12:40:27 -0400

    Get word counts for all words in corpus

--- small/search_small.py
+++ small/search_small.py
@@ -1,11 +1,28 @@
-import pprint
+import pprint as pp
 import re
 import operator
 
-def count(terms):
+def count_all(doc_counts):
+	'''
+		Input: Word counts for each doc
+		Return: Word counts in corpus
+			e.g. 'the' appears in corpus x times
+	'''
+	counts = {}
+	for doc in doc_counts:
+		for term in doc:  # Go through each term in each doc, increment word count each time
+			if term[0] not in counts:
+				counts[term[0]] = term[1]
+			else:
+				counts[term[0]] += term[1]
+	counts = sorted(counts.items(), key=operator.itemgetter(1))
+	return counts
+
+def count_doc(terms):
 	'''
 		Input: List of words
-		Output: Word counts
+		Return: Word counts
+			e.g. 'the' appears in this doc, x times
 	'''
 	counts = {}
 	for term in terms:
@@ -33,13 +50,14 @@ def parse(doc):
 def main():
 	docs = ["doc1","doc2","doc3","doc4","doc5"]
 	docs = ["./"+i for i in docs]
-	terms = []  # List of documents; each doc is list of terms
-	for i in docs:
-		terms.append(parse(i))
-	# pprint.pprint(terms)
-	for t in terms: # Get word count for all docs
-		pprint.pprint(count(t)[-5:])
-
-
+	doc_terms = []  # List of documents; each doc is list of terms
+	doc_term_counts = []
+	for d in docs: # Build list of terms for each doc
+		doc_terms.append(parse(d))
+	for t in doc_terms: # Get word count for all docs
+		doc_term_counts.append(count_doc(t))
+	corpus_counts = count_all(doc_term_counts)
+	# pp.pprint(corpus_counts)
+	pp.pprint(doc_term_counts[0])
 
 main()
