9cc0bf2cfe1a67b61e450ac5c19e050b2ebd1624
sh999 <sh999gh@gmail.com>
2016-11-01 07:45:58 -0400

    Get pdf list associated with site linking it, though only 1 per site

--- pdf_list.py
+++ pdf_list.py
@@ -10,17 +10,41 @@ def get_urls_from_feed(input_file):
 	'''
 	pages = []
 	counter = 0
+	pages_and_pdf = {}
 	with open(input_file) as f:
 		for line in f:
 			line = json.loads(line)	
-			site = [x.encode('ascii','ignore') for x in line['linkedurls'] 	]
-			site = [x for x in site if x[-4:] == ".pdf"]
-			pages.extend(site)
+			pdf_site = [site.encode('ascii','ignore') for site in line['linkedurls'] 	]
+			pdf_site = [site for site in pdf_site if site[-4:] == ".pdf"]
+			pages.extend(pdf_site)
+
 	pages = set(pages)
 	pages = list(pages)
 	return pages
+def get_urls_from_feed2(input_file):
+	'''
+		Input: Path to json file with url link
+		Return: List of urls
+	'''
+	pages = []
+	counter = 0
+	pages_and_pdf = {}
+	with open(input_file) as f:
+		for line in f:
+			line = json.loads(line)	
+			# pdf_site = [site.encode('ascii','ignore') for site in line['linkedurls'] 	]
+			# pdf_site = [site for site in pdf_site if site[-4:] == ".pdf"]
+			# pages.extend(pdf_site)
+			for site in line['linkedurls']:
+
+				if site[-4:] == ".pdf":
+					pages_and_pdf[line['url'].encode('ascii','ignore')] = site.encode('ascii','ignore')
+
+	return pages_and_pdf
 
 path = "sitegraph-engr-730pm.json"
+# path = "sitegraph-engr-9pm.json"
+
 urls = get_urls_from_feed(path)
 # pprint(urls[0][-4:])
 pprint(urls)
